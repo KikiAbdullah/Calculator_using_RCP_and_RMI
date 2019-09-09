e84db389d203ee9e3b9145c300da9f3113d786fd
BravoNatalie <natalie.bravo@outlook.com>
2019-09-09 19:56:59 -0300

    RMI calculator

--- RMI_Calculator/calculator_Client.py
+++ RMI_Calculator/calculator_Client.py
@@ -1,19 +1,5 @@
-import xmlrpc.client as xc
+import Pyro4
 from pyparsing import (Literal, CaselessLiteral, Word, Combine, Group, Optional, ZeroOrMore, Forward, nums, alphas, oneOf)
-from subprocess import Popen
-
-addHost = xc.ServerProxy("http://localhost:8001/")
-subHost = xc.ServerProxy("http://localhost:8002/")
-multHost = xc.ServerProxy("http://localhost:8003/")
-divHost = xc.ServerProxy("http://localhost:8004/")
-
-# def startHosts():
-#     Popen("pyhton3 addition_Host.py")
-#     Popen("pyhton3 subtraction_Host.py")
-#     Popen("pyhton3 multiplication_Host.py")
-#     Popen("pyhton3 division_Host.py")
-
-
 
 class ExpressionParser(object):
 
@@ -53,10 +39,16 @@ class ExpressionParser(object):
         
         self.bnf = expr
         # map operator symbols to corresponding arithmetic operations
-        self.opn= { '+': (lambda n1, n2: addHost.add(n1,n2)),
-                    '-': (lambda n1, n2: subHost.sub(n1,n2)),
-                    '*': (lambda n1, n2: multHost.mult(n1,n2)),
-                    '/': (lambda n1, n2: divHost.div(n1,n2)) }
+
+        #ns = Pyro4.locateNS()
+        #uri = ns.lookup('calculator_Server')
+        #obj = Pyro4.Proxy(uri)
+        obj = Pyro4.core.Proxy('PYRO:Calculator@' + 'localhost' + ':8000')
+
+        self.opn= { '+': (lambda n1, n2: obj.add(n1,n2)),
+                    '-': (lambda n1, n2: obj.sub(n1,n2)),
+                    '*': (lambda n1, n2: obj.mult(n1,n2)),
+                    '/': (lambda n1, n2: obj.div(n1,n2)) }
         
     def evalStack(self, s):
         op = s.pop()
@@ -77,11 +69,10 @@ class ExpressionParser(object):
         value = self.evalStack(self.exprStack[:])
         return value
 
+
 if __name__ == "__main__":
     # startHosts()
     expression = input("Enter an expression: ")
     calculator = ExpressionParser()
     result = calculator.evalExpr(expression.replace(" ", ""))
     print(result)
-    #result = calculator.evalExpr('5*10/(2+4)')
-    
